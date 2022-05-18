abc854747ae351433dc6fa962059e2c9e623918c
mhlee <mihyun0921@gmail.com>
2022-05-18 14:55:53 +0900

    drf-yasf 설정

--- README.md
+++ README.md
@@ -6,3 +6,8 @@ python manage.py dumpdata movies -o movies.json
 python manage.py migrate
 python manage.py loaddata movies.json
 ```
+
+# Django Restful API 문서 확인
+```
+http://127.0.0.1:8000/api/v1/swagger/
+```
--- movies/urls.py
+++ movies/urls.py
@@ -3,6 +3,16 @@ from . import views
 from drf_yasg.views import get_schema_view
 from drf_yasg import openapi
 
+schema_view = get_schema_view(
+   openapi.Info(
+      title="ONESHOT API",
+      default_version='v1',
+      description="ONESHOT API description",
+   ),
+   public=True,
+)
+
 urlpatterns = [
     path('movie/popular/', views.movie_popular),
+    path('swagger/', schema_view.with_ui('redoc')),
 ]
