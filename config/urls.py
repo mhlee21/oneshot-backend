7ece913d64cef6b26a648ded72f13e87b339a123
mhlee <mihyun0921@gmail.com>
2022-05-19 20:35:23 +0900

    signup CSRF Token error 해결

--- config/urls.py
+++ config/urls.py
@@ -36,6 +36,6 @@ urlpatterns = [
     path('api/v1/shots/', include('shots.urls')),
     path('api/v1/accounts/', include('accounts.urls')),
     path('api/v1/accounts/', include('dj_rest_auth.urls')),
+    path('api/v1/accounts/signup/', include('dj_rest_auth.registration.urls')),
     path('api/v1/accounts/', include('allauth.urls')),
-    path('api/v1/accounts/signup', include('dj_rest_auth.registration.urls')),
 ]
