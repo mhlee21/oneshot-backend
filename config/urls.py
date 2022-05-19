294d770527f02e62a468ce8bcdbd4ea1f33c3e7e
mhlee <mihyun0921@gmail.com>
2022-05-19 10:01:05 +0900

    permissions 설정 시 drf-yasg 접근 불가능한 문제 해결

--- config/urls.py
+++ config/urls.py
@@ -15,6 +15,7 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path,include
+from rest_framework import permissions
 from drf_yasg.views import get_schema_view
 from drf_yasg import openapi
 
@@ -25,6 +26,7 @@ schema_view = get_schema_view(
       description="ONESHOT API description",
    ),
    public=True,
+   permission_classes=[permissions.AllowAny],
 )
 
 urlpatterns = [
