c949afb8bf19174a40ed2261a4bc7d3dad179928
mhlee <mihyun0921@gmail.com>
2022-05-21 14:52:23 +0900

    media 파일 사용을 위한 urls.py 설정 추가

--- config/urls.py
+++ config/urls.py
@@ -14,6 +14,8 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
+from django.conf.urls.static import static
+from django.conf import settings
 from django.urls import path,include
 from rest_framework import permissions
 from drf_yasg.views import get_schema_view
@@ -39,3 +41,5 @@ urlpatterns = [
     path('api/v1/accounts/signup/', include('dj_rest_auth.registration.urls')),
     path('api/v1/accounts/', include('allauth.urls')),
 ]
+urlpatterns += \
+    static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
