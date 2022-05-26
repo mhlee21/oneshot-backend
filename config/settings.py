ecd9fda24a73182428ae80a0d604764db38daeb3
mhlee <mihyun0921@gmail.com>
2022-05-26 10:56:35 +0900

    tk error

--- config/settings.py
+++ config/settings.py
@@ -13,6 +13,8 @@ https://docs.djangoproject.com/en/4.0/ref/settings/
 from pathlib import Path
 from decouple import config
 import os
+matplotlib.use('Agg')
+import matplotlib.pyplot as plt
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
